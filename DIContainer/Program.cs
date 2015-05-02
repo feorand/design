eb9f5cddf6ee4ca6129a248ae1b96059994194e7
Pavel Prokofyev <pavel.prokofyev@outlook.com>
2015-05-02 15:56:05 +0500

    added Container

--- DIContainer/Program.cs
+++ DIContainer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using DIContainer.Commands;
+using Ninject;
 
 namespace DIContainer
 {
@@ -18,11 +19,12 @@ namespace DIContainer
 
         static void Main(string[] args)
         {
-            var arguments = new CommandLineArgs(args);
-            var printTime = new PrintTimeCommand();
-            var timer = new TimerCommand(arguments);
-            var commands = new ICommand[] { printTime, timer };
-            new Program(arguments, commands).Run();
+            var container = new StandardKernel();
+            container.Bind<ICommand>().To<TimerCommand>();
+            container.Bind<ICommand>().To<PrintTimeCommand>();
+            container.Bind<ICommand>().To<HelpCommand>();
+            container.Bind<CommandLineArgs>().ToMethod(x => new CommandLineArgs(args));
+            container.Get<Program>().Run();
         }
 
         public void Run()
